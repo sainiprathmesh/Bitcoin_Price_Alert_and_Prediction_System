49b73bcd0483ac3dc8b9aac0ccf11d4af3ec142a
Prathmesh Kumar Saini <prathmesh.saini_cs18@gla.ac.in>
2020-10-27 17:47:37 +0530

    test train spliy done

--- prediction/machine_learning_models.py
+++ prediction/machine_learning_models.py
@@ -1,11 +1,20 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("bitcoin_usd.csv")
 
 df = df.drop(labels=['_id', 'time'], axis=1)
 
-sns.pairplot(df)
+print(sns.pairplot(df))
 
-df.corr()
-sns.heatmap(df.corr())
+print(df.corr())
+print(sns.heatmap(df.corr()))
+
+print(plt.plot(df))
+
+X = df.loc[:, ['close', 'low', 'open']]
+Y = df.loc[:, 'high']
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
