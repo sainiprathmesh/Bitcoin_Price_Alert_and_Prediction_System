35dbe302174887316d2c8e2f257896a10b222b13
Prathmesh Kumar Saini <prathmesh.saini_cs18@gla.ac.in>
2020-10-27 19:01:42 +0530

    LSTM implemented

--- prediction/deep_learning_models.py
+++ prediction/deep_learning_models.py
@@ -7,7 +7,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import GRU
+from tensorflow.keras.layers import LSTM, GRU
 from tensorflow.keras.models import Sequential
 
 df = pd.read_csv('bitcoin_usd.csv')
@@ -62,7 +62,7 @@ model1.compile(optimizer='adam', loss='mse')
 model1.fit(X_train, y_train, epochs=100, verbose=1)
 eg_time = time.time()
 
-### Lets Do the prediction and check performance metrics
+# Lets Do the prediction and check performance metrics
 train_predict = model1.predict(X_train)
 test_predict = model1.predict(X_test)
 print("Train Predict: ", train_predict)
@@ -73,16 +73,62 @@ print("Execution time: ", eg_time - sg_time)
 from sklearn.metrics import mean_squared_error
 
 mse0 = mean_squared_error(y_train, train_predict)
-print("mse0: ", mse0)
+print("mse_train_gru: ", mse0)
 
 # Calculate MSE performance metrics
 from sklearn.metrics import mean_squared_error
 
 mse1 = mean_squared_error(y_test, test_predict)
-print("mse1: ", mse1)
+print("mse_test_gru: ", mse1)
 
 # Calculate r2 performance metrics
 from sklearn.metrics import r2_score
 
 r1 = r2_score(y_test, test_predict)
-print("r1: ", r1)
+print("r2_gru: ", r1)
+
+# define model2 LSTM
+sl_time = time.time()
+model2 = Sequential()
+model2.add(LSTM(100, activation='linear', return_sequences=True, input_shape=(3, 1)))
+model2.add(LSTM(50, activation='linear'))
+model2.add(Dense(1))
+model2.compile(optimizer='adam', loss='mse')
+# fit model
+model2.fit(X_train, y_train, epochs=100, verbose=1)
+el_time = time.time()
+
+# Lets Do the prediction and check performance metrics
+train_predict = model2.predict(X_train)
+test_predict = model2.predict(X_test)
+print("Train Predict: ", train_predict)
+print("Test Predict: ", test_predict)
+print("Execution Time: ", el_time - sl_time)
+
+# Calculate MSE performance metrics
+from sklearn.metrics import mean_squared_error
+
+mse_train_lstm = mean_squared_error(y_train, train_predict)
+print("mse_train_lstm: ", mse_train_lstm)
+
+# Calculate MSE performance metrics
+from sklearn.metrics import mean_squared_error
+
+mse_test_lstm = mean_squared_error(y_test, test_predict)
+print("mse_test_lstm: ", mse_test_lstm)
+
+# Calculate r2 performance metrics
+from sklearn.metrics import r2_score
+
+r2 = r2_score(y_test, test_predict)
+print("r2_lstm: ", r2)
+
+# plot
+plt.plot(scaler.inverse_transform(y_test))
+plt.plot(scaler.inverse_transform(test_predict))
+plt.xlim(200, 300)
+plt.show()
+
+df_model = pd.DataFrame({'Model_Applied': ['GRU', 'LSTM'], 'MSE': [mse1, mse_test_lstm], 'R2': [r1, r2],
+                         'Execution Time': [eg_time - sg_time, el_time - sl_time]})
+print(df_model)
